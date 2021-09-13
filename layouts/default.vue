1a22755b0c3db2d8d096281ab67e1790c7b05275
Lionelchen <chenlinchuan@me.com>
2021-09-13 23:04:23 +0800

    apply template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,116 +1,407 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+  <div id="app">
+    <v-app>
+      <v-app-bar
+        app
+        color="white"
+        height="100"
+      >
+        <v-avatar
+          class="mr-3"
+          color="grey lighten-5"
+          size="70"
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+          <v-img
+            contain
+            max-height="70%"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          />
+        </v-avatar>
+
+        <v-toolbar-title class="font-weight-black headline">
+          VUETIFY
+        </v-toolbar-title>
+      </v-app-bar>
+
+      <v-main>
+        <section id="hero">
+          <v-row no-gutters>
+            <v-img
+              :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+              src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
+            >
+              <v-theme-provider dark>
+                <v-container fill-height>
+                  <v-row
+                    align="center"
+                    class="white--text mx-auto"
+                    justify="center"
+                  >
+                    <v-col
+                      class="white--text text-center"
+                      cols="12"
+                      tag="h1"
+                    >
+                      <span
+                        :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                        class="font-weight-light"
+                      >
+                        WELCOME TO
+                      </span>
+
+                      <br>
+
+                      <span
+                        :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                        class="font-weight-black"
+                      >
+                        VUETIFY
+                      </span>
+                    </v-col>
+
+                    <v-btn
+                      class="align-self-end"
+                      fab
+                      outlined
+                      @click="$vuetify.goTo('#about-me')"
+                    >
+                      <v-icon>mdi-chevron-double-down</v-icon>
+                    </v-btn>
+                  </v-row>
+                </v-container>
+              </v-theme-provider>
+            </v-img>
+          </v-row>
+        </section>
+
+        <section id="about-me">
+          <div class="py-12" />
+
+          <v-container class="text-center">
+            <h2 class="display-2 font-weight-bold mb-3">
+              ABOUT ME
+            </h2>
+
+            <v-responsive
+              class="mx-auto mb-8"
+              width="56"
+            >
+              <v-divider class="mb-1" />
+
+              <v-divider />
+            </v-responsive>
+
+            <v-responsive
+              class="mx-auto title font-weight-light mb-8"
+              max-width="720"
+            >
+              Vuetify is the #1 component library for Vue.js and has been in active development since 2016. The goal of the project is to provide users with everything that is needed to build rich and engaging web applications using the Material Design specification. It accomplishes that with a consistent update cycle, Long-term Support (LTS) for previous versions, responsive community engagement, a vast ecosystem of resources and a dedication to quality components.
+            </v-responsive>
+
+            <v-avatar
+              class="elevation-12 mb-12"
+              size="128"
+            >
+              <v-img src="https://cdn.vuetifyjs.com/images/john.png" />
+            </v-avatar>
+
+            <div />
+
+            <v-btn
+              color="grey"
+              href="https://vuetifyjs.com"
+              outlined
+              large
+            >
+              <span class="grey--text text--darken-1 font-weight-bold">
+                Vuetify Documentation
+              </span>
+            </v-btn>
+          </v-container>
+
+          <div class="py-12" />
+        </section>
+
+        <section
+          id="features"
+          class="grey lighten-3"
+        >
+          <div class="py-12" />
+
+          <v-container class="text-center">
+            <h2 class="display-2 font-weight-bold mb-3">
+              VUETIFY FEATURES
+            </h2>
+
+            <v-responsive
+              class="mx-auto mb-12"
+              width="56"
+            >
+              <v-divider class="mb-1" />
+
+              <v-divider />
+            </v-responsive>
+
+            <v-row>
+              <v-col
+                v-for="({ icon, title, text }, i) in features"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <v-card
+                  class="py-12 px-4"
+                  color="grey lighten-5"
+                  flat
+                >
+                  <v-theme-provider dark>
+                    <div>
+                      <v-avatar
+                        color="primary"
+                        size="88"
+                      >
+                        <v-icon
+                          large
+                          v-text="icon"
+                        />
+                      </v-avatar>
+                    </div>
+                  </v-theme-provider>
+
+                  <v-card-title
+                    class="justify-center font-weight-black text-uppercase"
+                    v-text="title"
+                  />
+
+                  <v-card-text
+                    class="subtitle-1"
+                    v-text="text"
+                  />
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <div class="py-12" />
+        </section>
+
+        <section id="stats">
+          <v-parallax
+            :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+            src="https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          >
+            <v-container fill-height>
+              <v-row class="mx-auto">
+                <v-col
+                  v-for="[value, title] of stats"
+                  :key="title"
+                  cols="12"
+                  md="3"
+                >
+                  <div class="text-center">
+                    <div
+                      class="display-3 font-weight-black mb-4"
+                      v-text="value"
+                    />
+
+                    <div
+                      class="title font-weight-regular text-uppercase"
+                      v-text="title"
+                    />
+                  </div>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-parallax>
+        </section>
+
+        <section id="blog">
+          <div class="py-12" />
+
+          <v-container>
+            <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+              Blog
+            </h2>
+
+            <v-responsive
+              class="mx-auto mb-12"
+              width="56"
+            >
+              <v-divider class="mb-1" />
+
+              <v-divider />
+            </v-responsive>
+
+            <v-row>
+              <v-col
+                v-for="({ src, text, title }, i) in articles"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <v-img
+                  :src="src"
+                  class="mb-4"
+                  height="275"
+                  max-width="100%"
+                />
+
+                <h3
+                  class="font-weight-black mb-4 text-uppercase"
+                  v-text="title"
+                />
+
+                <div
+                  class="title font-weight-light mb-5"
+                  v-text="text"
+                />
+
+                <v-btn
+                  class="ml-n4 font-weight-black"
+                  text
+                >
+                  Continue Reading
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <div class="py-12" />
+        </section>
+
+        <v-sheet
+          id="contact"
+          color="#333333"
+          dark
+          tag="section"
+          tile
+        >
+          <div class="py-12" />
+
+          <v-container>
+            <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+              Contact Me
+            </h2>
+
+            <v-responsive
+              class="mx-auto mb-12"
+              width="56"
+            >
+              <v-divider class="mb-1" />
+
+              <v-divider />
+            </v-responsive>
+
+            <v-theme-provider light>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    flat
+                    label="Name*"
+                    solo
+                  />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-text-field
+                    flat
+                    label="Email*"
+                    solo
+                  />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-text-field
+                    flat
+                    label="Subject*"
+                    solo
+                  />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-textarea
+                    flat
+                    label="Message*"
+                    solo
+                  />
+                </v-col>
+
+                <v-col
+                  class="mx-auto"
+                  cols="auto"
+                >
+                  <v-btn
+                    color="accent"
+                    x-large
+                  >
+                    Submit
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-theme-provider>
+          </v-container>
+
+          <div class="py-12" />
+        </v-sheet>
+      </v-main>
+
+      <v-footer
+        class="justify-center"
+        color="#292929"
+        height="100"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+        <div class="title font-weight-light grey--text text--lighten-1 text-center">
+          &copy; {{ (new Date()).getFullYear() }} — Vuetify, LLC — Made with 💜 by John Leider
+        </div>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
+      articles: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          src: 'https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          title: 'Mobile first & Responsive',
+          text: 'Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum.'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          src: 'https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          title: 'Think outside the box',
+          text: 'Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh.'
+        },
+        {
+          src: 'https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80',
+          title: 'Small changes, big difference',
+          text: 'Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget.'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      features: [
+        {
+          icon: 'mdi-account-group-outline',
+          title: 'Vibrant Community',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam'
+        },
+        {
+          icon: 'mdi-update',
+          title: 'Frequent Updates',
+          text: 'Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit.'
+        },
+        {
+          icon: 'mdi-shield-outline',
+          title: 'Long-term Support',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam'
+        }
+      ],
+      stats: [
+        ['24k', 'Github Stars'],
+        ['330+', 'Releases'],
+        ['1m', 'Downloads/mo'],
+        ['5m', 'Total Downloads']
+      ]
     }
   }
 }
